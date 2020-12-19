ef7b7684cc57a47711a63c8802c42784efdc1cfc
Marisabel <72731147+immarisabel@users.noreply.github.com>
2020-12-19 14:50:31 +0100

    window
    started working on the window just for fun and a break

--- src/LingoGame.java
+++ src/LingoGame.java
@@ -1,13 +1,71 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
 import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class LingoGame
 {
+	// GAME WINDOW SET UP
+	JFrame window;
+	Container con;
+	Font normal = new Font("Tahoma", Font.PLAIN, 16);
+	JPanel titleNamePanel;
+	JLabel titleNameLabel;
+	Font titleFont = new Font("Arial", Font.PLAIN, 28);
+
+	JPanel startButtonPanel;
+	JButton startButton;
+
+	public LingoGame()
+	{
+		// WINDOW
+		window = new JFrame();
+		window.setSize(400, 400);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.getContentPane().setBackground(Color.DARK_GRAY);
+		window.setLayout(null);
+		con = window.getContentPane();
+
+		// CONTAINER
+		titleNamePanel = new JPanel();
+		titleNamePanel.setBounds(50, 80, 300, 50);
+		titleNamePanel.setBackground(Color.DARK_GRAY);
+		titleNameLabel = new JLabel("Lets Lingo");
+		titleNameLabel.setForeground(Color.white);
+		titleNameLabel.setFont(titleFont);
+
+		startButtonPanel = new JPanel();
+		startButtonPanel.setBounds(100, 200, 200, 50);
+		startButtonPanel.setBackground(Color.DARK_GRAY);
+
+		startButton = new JButton("START");
+		startButton.setBackground(Color.PINK);
+		startButton.setForeground(Color.DARK_GRAY);
+		startButton.setFont(normal);
+
+		// Add title inside panel
+		titleNamePanel.add(titleNameLabel);
+		startButtonPanel.add(startButton);
+
+		con.add(titleNamePanel);
+		con.add(startButtonPanel);
+
+		window.setVisible(true); // IMPORTANT! even if we create if, we need ot make it show with this
+
+	}
 
 	public static String[] guesses = new String[6];
 	public static String wrongGuess;
 
 	public static void main(String[] args)
 	{
+		new LingoGame();
+
 		@SuppressWarnings("resource")
 		Scanner play = new Scanner(System.in);
 		System.out.println("\nWanna play ? \n >>> #1 YES\n >>> #2 NO\n type 1 or 2");
@@ -32,4 +90,5 @@ public class LingoGame
 		}
 
 	}
+
 }
