00a06e741c07191d90ecc52a7bbaec36ca0d1e5d
Marisabel <72731147+immarisabel@users.noreply.github.com>
2020-12-27 16:33:13 +0100

    working on file reader
    might not work for this... need to look up a tutorial to check if it does work

--- src/Scores.java
+++ src/Scores.java
@@ -2,101 +2,90 @@
 // TO USE:
 
 
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.stream.Stream;
 
 public class Scores
 {
 	// VARIABLES
+
 	static String date = setDate();
 	static int finalScore = 0;
 	static int highScore = 0;
 	static String filePath ="scores.txt";
 
+
 	//GET RESULT FROM GAME
-	public static int newHighScore() {
-		finalScore = 15;
+	public static int newScore() {
+		finalScore = 21;
 		return finalScore;
+	}
+
+// METHOD to read high score from file
+	public static String getHighScore(){
+	int n = 1;
+	String strscore = "";
+	try(Stream<String> lines = Files.lines(Paths.get(filePath))) {
+	strscore = lines.skip(n).findFirst().get();
+	}
+	catch(IOException e){
+		System.out.println(e);
+		}
+		return strscore;
+	}
+
+// METHOD to return highscore as int // data is being deleted everytime it is read.
+	//reading: https://stackoverflow.com/questions/35887667/why-does-introducing-a-filewriter-delete-all-the-content-in-the-file
+//The problem is that you're trying to write to the file while you're reading from it. A better solution would be to create a second file, put the transformed data into it, then replace the first file with it when you're done. Or if you don't want to do that, read all of the data out of the file first, then open it for writing and write the transformed data.
+	public static int HighScore() {
+		highScore = 30;
+		System.out.println("DEBUG: High score is set to " + highScore);
+
+
+		String strhighScore = getHighScore();
+		System.out.println("DEBUG: High score string is set to " + strhighScore);
+		int noninthighScore = Integer.parseInt(strhighScore);
+		System.out.println("DEBUG: High score integer is set to " + noninthighScore);
+		highScore = noninthighScore;
+
+		return highScore;
 	}
 
 	//TEST THE RESULTS
 	public static void main(String[] args) throws FileNotFoundException {
-	// TODO delete when working code and return the Home.java to main
+/* TODO delete when working code and return the Home.java to main */
 	System.out.println("DATE : " +date);
-	System.out.println("FINAL SCORE : " + newHighScore());
-	System.out.println("HIGH SCORE : " + readHighScore());
-		if (newHighScore() > highScore){
+	System.out.println("FINAL SCORE : " + newScore());
+	System.out.println("HIGH SCORE : " + HighScore());
+setHighScore();
+
+
+
+
+//TODO GOOD! DON'T TOUCH!///////////////////////////////////////////
+
+
+// FLOW OK! (To be used in main game file)
+		if (newScore() > HighScore()){
 			System.out.println("New high score!");
-			setHighScore();}
+			setHighScore();
+		}
 		else {System.out.println("your scores was ok.");}
-
-	//setHighScore();
-}
-//TODO load date as well
-	public static void readHighScoreDate( ) {
-		String lineDate;
-		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-
-				br.readLine();
-			lineDate = br.readLine();
-			System.out.println("DATE IN FILE");
-
-			System.out.println(lineDate);
-		}
-		catch(IOException e){
-			System.out.println(e);
-		}
 }
 
-//TODO add this HS to variable initiator
-		public static String readHighScore() {
-			int score = 1; // int score
-
-			String line = null;
-			try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-				for (int i = 0; i < score; i++)
-					br.readLine();
-				line = br.readLine();
-				highScore = Integer.parseInt(line);
-						}
-			catch(IOException e){
-				System.out.println(e);
-			}
-
-			return line;
- 	}
-
-//	public static void createFile() throws IOException {
-//
-//		try {
-//			File myObj = new File("scores.txt");
-//			if (myObj.createNewFile()) {
-//				System.out.println("File created: " + myObj.getName());
-//			} else {
-//				System.out.println("File already exists.");
-//			}
-//		} catch (IOException e) {
-//			System.out.println("An error occurred.");
-//			e.printStackTrace();
-//		}
-//
-//		FileWriter finalScore = new FileWriter("scores.txt");
-//		PrintWriter scoreObject = new PrintWriter(finalScore);
-//
-//		scoreObject.println(finalScore);
-//		scoreObject.println(date);
-//
-//		System.out.println("Score updated!");
-//	}
-
-	public static void setHighScore( )
+// METHOD to write high score to file
+	public static void setHighScore()
 		{
 			try {
 			FileWriter myWriter = new FileWriter(filePath);
 			myWriter.write(date+ "\n" );
-			myWriter.write(
-					highScore);
+			myWriter.write(Integer.toString(HighScore()));
 			myWriter.close();
 			System.out.println("Successfully wrote to the file.");
 		} catch (IOException e) {
@@ -106,11 +95,6 @@ public class Scores
 
 	}
 
-	public static void resetHighScore(){}
-
-
-
-	//GOOD! DON'T TOUCH!
 	public static String setDate() {
 
 		// define date pattern
