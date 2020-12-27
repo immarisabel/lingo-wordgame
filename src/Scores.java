06ca12d314099a5f1e3a1d486323e19ed2c5d0f2
Marisabel <72731147+immarisabel@users.noreply.github.com>
2020-12-27 14:39:04 +0100

    progress
    Doing progress on the reader of the file but fighting with int vs String.

--- src/Scores.java
+++ src/Scores.java
@@ -1,54 +1,102 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.format.DateTimeFormatter;
-import java.time.LocalDateTime;
-
 // TO USE: System.out.println(dtf.format(now)); <- TIME DATE
 // TO USE:
 
 
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Scores
 {
 	// VARIABLES
-	static int score = 0;
-	static LocalDateTime now = LocalDateTime.now();
-	static String Name = null;
-public static void main(String[] args){
+	static String date = setDate();
+	static int finalScore = 0;
+	static int highScore = 0;
+	static String filePath ="scores.txt";
 
-
-	scoreWriter();
-}
-
-	public static void createFile(){
-
-		try {
-			File myObj = new File("scores.txt");
-			if (myObj.createNewFile()) {
-				System.out.println("File created: " + myObj.getName());
-			} else {
-				System.out.println("File already exists.");
-			}
-		} catch (IOException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
-
-
-
-//		FileWriter newScore = new FileWriter("scores.txt");
-//		PrintWriter scoreObject = new PrintWriter(newScore);
-//
-//		scoreObject.println(score);
-//		scoreObject.println(Scores.Timestamp(dtf.format(now)));
-//		scoreObject.println(name);
-//		System.out.println("Score updated!");
+	//GET RESULT FROM GAME
+	public static int newHighScore() {
+		finalScore = 15;
+		return finalScore;
 	}
 
-	public static void scoreWriter(){
-		try {
-			FileWriter myWriter = new FileWriter("scores.txt");
-			myWriter.write(Scores.Timestamp(Scores.Timestamp(dtf.format(now))));
+	//TEST THE RESULTS
+	public static void main(String[] args) throws FileNotFoundException {
+	// TODO delete when working code and return the Home.java to main
+	System.out.println("DATE : " +date);
+	System.out.println("FINAL SCORE : " + newHighScore());
+	System.out.println("HIGH SCORE : " + readHighScore());
+		if (newHighScore() > highScore){
+			System.out.println("New high score!");
+			setHighScore();}
+		else {System.out.println("your scores was ok.");}
+
+	//setHighScore();
+}
+//TODO load date as well
+	public static void readHighScoreDate( ) {
+		String lineDate;
+		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+				br.readLine();
+			lineDate = br.readLine();
+			System.out.println("DATE IN FILE");
+
+			System.out.println(lineDate);
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+}
+
+//TODO add this HS to variable initiator
+		public static String readHighScore() {
+			int score = 1; // int score
+
+			String line = null;
+			try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+				for (int i = 0; i < score; i++)
+					br.readLine();
+				line = br.readLine();
+				highScore = Integer.parseInt(line);
+						}
+			catch(IOException e){
+				System.out.println(e);
+			}
+
+			return line;
+ 	}
+
+//	public static void createFile() throws IOException {
+//
+//		try {
+//			File myObj = new File("scores.txt");
+//			if (myObj.createNewFile()) {
+//				System.out.println("File created: " + myObj.getName());
+//			} else {
+//				System.out.println("File already exists.");
+//			}
+//		} catch (IOException e) {
+//			System.out.println("An error occurred.");
+//			e.printStackTrace();
+//		}
+//
+//		FileWriter finalScore = new FileWriter("scores.txt");
+//		PrintWriter scoreObject = new PrintWriter(finalScore);
+//
+//		scoreObject.println(finalScore);
+//		scoreObject.println(date);
+//
+//		System.out.println("Score updated!");
+//	}
+
+	public static void setHighScore( )
+		{
+			try {
+			FileWriter myWriter = new FileWriter(filePath);
+			myWriter.write(date+ "\n" );
+			myWriter.write(
+					highScore);
 			myWriter.close();
 			System.out.println("Successfully wrote to the file.");
 		} catch (IOException e) {
@@ -58,15 +106,32 @@ public static void main(String[] args){
 
 	}
 
-	public static void Timestamp (String[] args) {
+	public static void resetHighScore(){}
 
-		LocalDateTime now = LocalDateTime.now();
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy at HH:mm");
-		System.out.println(dtf.format(now));
+
+
+	//GOOD! DON'T TOUCH!
+	public static String setDate() {
+
+		// define date pattern
+		String pattern = "yyyy-MM-dd";
+
+// declare that the variable is a date
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+
+// format the date
+		date = sdf.format(new Date());
+		return date;
 
 	}
 
 	}
+
+
+
+
+
+
 
 
 
